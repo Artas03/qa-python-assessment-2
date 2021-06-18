309d0153523c8eb3e62c7855982744f1b166b21f
Artas <gillartas@googlemail.com>
2021-06-18 10:35:33 +0000

    completed question one/two/three/four/five/six/seven

--- pr.py
+++ pr.py
@@ -1,5 +1,2 @@
-def four(string1, string2):
-    list_1 = [string1.split()]
-    list_2 = [string2.split()]
-
-print(four('hello','meow'), list_1)
+list = ['i','l','o','v','e','p','y']
+print(list[-2:])

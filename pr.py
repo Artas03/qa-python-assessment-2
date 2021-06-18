dd1fbb598783c886e2b0df1cb660fe57c5856891
Artas <gillartas@googlemail.com>
2021-06-18 10:44:28 +0000

    completed question one/two/three/four/five/six/seven/nine

--- pr.py
+++ pr.py
@@ -1,2 +1,6 @@
-list = ['i','l','o','v','e','p','y']
-print(list[-2:])
+def nine(string1, string2):
+    string1 = "tree"
+    string2 = "tiredest"
+    if set(string1) == set(string2):
+        print(f"Correct")
+    print(f"no")
